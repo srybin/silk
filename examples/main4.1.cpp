c812939e60419deeb10097983a589ebc2cc638b1
sergey rybin <ser.rybin@gmail.com>
2020-03-14 21:28:45 +0300

    Demo task runtime 4.1 has its own namespace (demo_runtime_4_1).

--- examples/main4.1.cpp
+++ examples/main4.1.cpp
@@ -3,36 +3,36 @@
 #include <iostream>
 #include "./taskruntime4.1.h"
 
-silk__coro<int> c2(const int i) {
-	co_await silk__yield();
+silk::demo_runtime_4_1::task<int> c2(const int i) {
+	co_await silk::demo_runtime_4_1::yield();
 
 	co_return i;
 }
 
-silk__coro<int> c3(const int i) {
+silk::demo_runtime_4_1::task<int> c3(const int i) {
 	auto r = co_await c2(i);
 
 	co_return r + 1;
 }
 
-silk__coro<int> c33(const int i) {
+silk::demo_runtime_4_1::task<int> c33(const int i) {
 	return c3(i);
 }
 
-silk__coro<> c31() {
-	silk__coro<int> c0 = c3(2);
+silk::demo_runtime_4_1::task<> c31() {
+	silk::demo_runtime_4_1::task<int> c0 = c3(2);
 
 	auto r = co_await c0;
 
-	printf("%d c31() -> %d\n", silk__current_worker_id, r);
+	printf("%d c31() -> %d\n", silk::current_worker_id, r);
 }
 
 std::atomic<int> count;
 
-silk__independed_coro c0() {
-	co_await silk__yield();
+silk::demo_runtime_4_1::independed_task c0() {
+	co_await silk::demo_runtime_4_1::yield();
 
-	silk__coro<int> c0 = c3( 2 );
+	silk::demo_runtime_4_1::task<int> c0 = c3( 2 );
 
 	auto r0 = co_await c3( 1 );
 
@@ -48,25 +48,25 @@ silk__independed_coro c0() {
 
 	auto r5 = co_await c33( 5 );
 
-	auto completes_synchronously = []() -> silk__coro<int> {
+	auto completes_synchronously = []() -> silk::demo_runtime_4_1::task<int> {
 		co_return 1;
 	};
 
 	int r6 = co_await completes_synchronously();
 
-	printf("%d c0() -> %d %d %d %d %d %d %d\n", silk__current_worker_id, r0, r1, r2, r3, r4, r5, r6);
+	printf("%d c0() -> %d %d %d %d %d %d %d\n", silk::current_worker_id, r0, r1, r2, r3, r4, r5, r6);
 
 	count.fetch_add(1, std::memory_order_acquire);
 }
 
 int main() {
-	silk__init_pool(silk__schedule, silk__makecontext);
+	silk::init_pool(silk::demo_runtime_4_1::schedule, silk::makecontext);
 
 	for (int i = 0; i < 1000000; i++) {
-		silk__spawn( c0() );
+		silk::demo_runtime_4_1::spawn( c0() );
 	}
 
-	silk__join_main_thread_2_pool(silk__schedule);
+	silk::join_main_thread_2_pool(silk::demo_runtime_4_1::schedule);
 	
 	sleep(3);
 
--- examples/taskruntime4.1.h
+++ examples/taskruntime4.1.h
@@ -3,165 +3,169 @@
 #include <sys/types.h>
 #include <sys/event.h>
 
-template<typename T> struct silk__coro_promise;
-template<typename T> struct silk__coro;
-struct silk__independed_coro;
-
-struct silk__coro_promise_base {
-	std::experimental::coroutine_handle<> continuation;
-};
-
-struct silk__final_awaitable {
-	bool await_ready() const noexcept { return false; }
-
-	template<typename T> void await_suspend(std::experimental::coroutine_handle<T> coro) {
-		coro.promise().continuation.resume();
-	}
-
-	void await_resume() noexcept {}
-};
-
-struct silk__frame : public silk__task {
-	std::experimental::coroutine_handle<> coro;
-
-	silk__frame(std::experimental::coroutine_handle<> c) : coro(c) {}
-};
-
-void silk__spawn(std::experimental::coroutine_handle<> coro) {
-	silk__spawn(silk__current_worker_id, (silk__task*) new silk__frame(coro));
-}
-
-template<typename T = void> struct silk__coro_awaitable {
-	silk__coro<T>& awaitable;
-
-	bool await_ready() noexcept { return awaitable.coro.done(); }
-
-	void await_suspend(std::experimental::coroutine_handle<> coro) noexcept {
-		awaitable.coro.promise().continuation = coro;
-		awaitable.coro.resume();
-	}
-
-	auto await_resume() noexcept { return awaitable.result(); }
-};
-
-template<typename T> struct silk__coro_promise : public silk__coro_promise_base {
-	std::exception_ptr e_;
-	T v_;
-
-	silk__coro<T> get_return_object() noexcept;
-
-	auto initial_suspend() { return std::experimental::suspend_always(); }
-
-	auto final_suspend() { return silk__final_awaitable{}; }
-
-	void unhandled_exception() { e_ = std::current_exception(); }
-
-	void return_value(const T v) { v_ = v; }
-
-	T result() {
-		if (e_) {
-			std::rethrow_exception(e_);
-		}
-
-		return v_;
-	}
-};
-
-template<> struct silk__coro_promise<void> : public silk__coro_promise_base {
-	silk__coro_promise() noexcept = default;
-	silk__coro<void> get_return_object() noexcept;
-	auto initial_suspend() { return std::experimental::suspend_always{}; }
-
-	auto final_suspend() { return silk__final_awaitable{}; }
-
-	void return_void() noexcept {}
-
-	std::exception_ptr e_;
-	void unhandled_exception() { e_ = std::current_exception(); }
-	void result() {
-		if (e_) {
-			std::rethrow_exception(e_);
-		}
-	}
-};
-
-template<typename T = void> struct silk__coro {
-	using promise_type = silk__coro_promise<T>;
-
-	std::experimental::coroutine_handle<silk__coro_promise<T>> coro;
-
-	silk__coro(std::experimental::coroutine_handle<silk__coro_promise<T>> c) : coro(c) {
-	}
-
-	~silk__coro() {
-		if (coro && coro.done())
-			coro.destroy();
-	}
-
-	const T result() { return coro.promise().result(); }
-
-	silk__coro_awaitable<T> operator co_await() { return silk__coro_awaitable<T> {*this}; }
-};
-
-template<typename T> silk__coro<T> silk__coro_promise<T>::get_return_object() noexcept {
-	return silk__coro<T> { std::experimental::coroutine_handle<silk__coro_promise>::from_promise(*this) };
-}
-
-inline silk__coro<void> silk__coro_promise<void>::get_return_object() noexcept {
-	return silk__coro<void> { std::experimental::coroutine_handle<silk__coro_promise>::from_promise(*this) };
-}
-
-struct silk__independed_coro_promise {
-	silk__independed_coro get_return_object() noexcept;
-	auto initial_suspend() { return std::experimental::suspend_always{}; }
-
-	auto final_suspend() { return std::experimental::suspend_never{}; }
-
-	void return_void() noexcept {}
-
-	std::exception_ptr e_;
-	void unhandled_exception() { e_ = std::current_exception(); }
-	void result() {
-		if (e_) {
-			std::rethrow_exception(e_);
-		}
-	}
-};
-
-struct silk__independed_coro {
-	using promise_type = silk__independed_coro_promise;
-
-	std::experimental::coroutine_handle<> coro;
-
-	silk__independed_coro(std::experimental::coroutine_handle<> c) : coro(c) { }
-};
-
-inline silk__independed_coro silk__independed_coro_promise::get_return_object() noexcept {
-	return silk__independed_coro{ std::experimental::coroutine_handle<silk__independed_coro_promise>::from_promise(*this) };
-}
-
-void silk__spawn(silk__independed_coro c) {
-	silk__spawn(c.coro);
-}
-
-void silk__schedule(silk__task* t) {
-	silk__frame* c = (silk__frame*)t;
-
-	c->coro.resume();
-
-	delete c;
-}
-
-struct silk__yield_awaitable {
-	bool await_ready() const noexcept { return false; }
-
-	template<typename T> void await_suspend(std::experimental::coroutine_handle<T> c) {
-		silk__spawn(c);
-	}
-
-	void await_resume() noexcept {}
-};
-
-auto silk__yield() {
-	return silk__yield_awaitable{};
+namespace silk {
+    namespace demo_runtime_4_1 {
+        template<typename T> struct task_promise;
+        template<typename T> struct task;
+        struct independed_task;
+        
+        struct task_promise_base {
+        	std::experimental::coroutine_handle<> continuation;
+        };
+        
+        struct final_awaitable {
+        	bool await_ready() const noexcept { return false; }
+        
+        	template<typename T> void await_suspend(std::experimental::coroutine_handle<T> coro) {
+        		coro.promise().continuation.resume();
+        	}
+        
+        	void await_resume() noexcept {}
+        };
+        
+        struct frame : public silk::task {
+        	std::experimental::coroutine_handle<> coro;
+        
+        	frame(std::experimental::coroutine_handle<> c) : coro(c) {}
+        };
+        
+        void spawn(std::experimental::coroutine_handle<> coro) {
+        	silk::spawn(silk::current_worker_id, (silk::task*) new frame(coro));
+        }
+        
+        template<typename T = void> struct task_awaitable {
+        	task<T>& awaitable;
+        
+        	bool await_ready() noexcept { return awaitable.coro.done(); }
+        
+        	void await_suspend(std::experimental::coroutine_handle<> coro) noexcept {
+        		awaitable.coro.promise().continuation = coro;
+        		awaitable.coro.resume();
+        	}
+        
+        	auto await_resume() noexcept { return awaitable.result(); }
+        };
+        
+        template<typename T> struct task_promise : public task_promise_base {
+        	std::exception_ptr e_;
+        	T v_;
+        
+        	task<T> get_return_object() noexcept;
+        
+        	auto initial_suspend() { return std::experimental::suspend_always(); }
+        
+        	auto final_suspend() { return final_awaitable{}; }
+        
+        	void unhandled_exception() { e_ = std::current_exception(); }
+        
+        	void return_value(const T v) { v_ = v; }
+        
+        	T result() {
+        		if (e_) {
+        			std::rethrow_exception(e_);
+        		}
+        
+        		return v_;
+        	}
+        };
+        
+        template<> struct task_promise<void> : public task_promise_base {
+        	task_promise() noexcept = default;
+        	task<void> get_return_object() noexcept;
+        	auto initial_suspend() { return std::experimental::suspend_always{}; }
+        
+        	auto final_suspend() { return final_awaitable{}; }
+        
+        	void return_void() noexcept {}
+        
+        	std::exception_ptr e_;
+        	void unhandled_exception() { e_ = std::current_exception(); }
+        	void result() {
+        		if (e_) {
+        			std::rethrow_exception(e_);
+        		}
+        	}
+        };
+        
+        template<typename T = void> struct task {
+        	using promise_type = task_promise<T>;
+        
+        	std::experimental::coroutine_handle<task_promise<T>> coro;
+        
+        	task(std::experimental::coroutine_handle<task_promise<T>> c) : coro(c) {
+        	}
+        
+        	~task() {
+        		if (coro && coro.done())
+        			coro.destroy();
+        	}
+        
+        	const T result() { return coro.promise().result(); }
+        
+        	task_awaitable<T> operator co_await() { return task_awaitable<T> {*this}; }
+        };
+        
+        template<typename T> task<T> task_promise<T>::get_return_object() noexcept {
+        	return task<T> { std::experimental::coroutine_handle<task_promise>::from_promise(*this) };
+        }
+        
+        inline task<void> task_promise<void>::get_return_object() noexcept {
+        	return task<void> { std::experimental::coroutine_handle<task_promise>::from_promise(*this) };
+        }
+        
+        struct independed_task_promise {
+        	independed_task get_return_object() noexcept;
+        	auto initial_suspend() { return std::experimental::suspend_always{}; }
+        
+        	auto final_suspend() { return std::experimental::suspend_never{}; }
+        
+        	void return_void() noexcept {}
+        
+        	std::exception_ptr e_;
+        	void unhandled_exception() { e_ = std::current_exception(); }
+        	void result() {
+        		if (e_) {
+        			std::rethrow_exception(e_);
+        		}
+        	}
+        };
+        
+        struct independed_task {
+        	using promise_type = independed_task_promise;
+        
+        	std::experimental::coroutine_handle<> coro;
+        
+        	independed_task(std::experimental::coroutine_handle<> c) : coro(c) { }
+        };
+        
+        inline independed_task independed_task_promise::get_return_object() noexcept {
+        	return independed_task{ std::experimental::coroutine_handle<independed_task_promise>::from_promise(*this) };
+        }
+        
+        void spawn(independed_task c) {
+        	spawn(c.coro);
+        }
+        
+        void schedule(silk::task* t) {
+        	frame* c = (frame*)t;
+        
+        	c->coro.resume();
+        
+        	delete c;
+        }
+        
+        struct yield_awaitable {
+        	bool await_ready() const noexcept { return false; }
+        
+        	template<typename T> void await_suspend(std::experimental::coroutine_handle<T> c) {
+        		spawn(c);
+        	}
+        
+        	void await_resume() noexcept {}
+        };
+        
+        auto yield() {
+        	return yield_awaitable{};
+        }   
+    }
 }
