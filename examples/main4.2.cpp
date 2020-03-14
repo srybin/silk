4edb791cf36f6a8dc817360f88496f5207cb1d90
sergey rybin <ser.rybin@gmail.com>
2020-03-14 21:39:15 +0300

    Demo task runtime 4.2 has its own namespace (demo_runtime_4_2).

--- examples/main4.2.cpp
+++ examples/main4.2.cpp
@@ -3,40 +3,40 @@
 #include <iostream>
 #include "./taskruntime4.2.h"
 
-silk__coro<int> c2(const int i) {
-	co_await silk__yield();
+silk::demo_runtime_4_2::task<int> c2(const int i) {
+	co_await silk::demo_runtime_4_2::yield();
 
 	co_return i;
 }
 
-silk__coro<int> c3(const int i) {
+silk::demo_runtime_4_2::task<int> c3(const int i) {
 	auto r = co_await c2(i);
 
 	co_return r + 1;
 }
 
-silk__coro<int> c33(const int i) {
+silk::demo_runtime_4_2::task<int> c33(const int i) {
 	return c3(i);
 }
 
-silk__coro<> c31() {
-	silk__coro<int> c0 = c3(2);
+silk::demo_runtime_4_2::task<> c31() {
+	silk::demo_runtime_4_2::task<int> c0 = c3(2);
 
-	silk__spawn(c0);
+	silk::demo_runtime_4_2::spawn(c0);
 
 	auto r = co_await c0;
 
-	printf("%d c31() -> %d\n", silk__current_worker_id, r);
+	printf("%d c31() -> %d\n", silk::current_worker_id, r);
 }
 
 std::atomic<int> count;
 
-silk__independed_coro c0() {
-	co_await silk__yield();
+silk::demo_runtime_4_2::independed_task c0() {
+	co_await silk::demo_runtime_4_2::yield();
 
-	silk__coro<int> c0 = c3( 2 );
+	silk::demo_runtime_4_2::task<int> c0 = c3( 2 );
 
-	silk__spawn( c0 );
+	silk::demo_runtime_4_2::spawn( c0 );
 
 	auto r0 = co_await c3( 1 );
 
@@ -44,7 +44,7 @@ silk__independed_coro c0() {
 
 	co_await c31();
 
-	silk__coro<> c01 = silk__spawn( c31() );
+	silk::demo_runtime_4_2::task<> c01 = silk::demo_runtime_4_2::spawn( c31() );
 
 	co_await c01;
 
@@ -52,11 +52,11 @@ silk__independed_coro c0() {
 
 	auto r3 = co_await c33( 4 );
 
-	silk__coro<int> c1 = silk__spawn( c33( 5 ) );
+	silk::demo_runtime_4_2::task<int> c1 = silk::demo_runtime_4_2::spawn( c33( 5 ) );
 
 	auto r4 = co_await c1;
 
-	auto completes_synchronously = []() -> silk__coro<int> {
+	auto completes_synchronously = []() -> silk::demo_runtime_4_2::task<int> {
 		co_return 1;
 	};
 
@@ -64,23 +64,23 @@ silk__independed_coro c0() {
 	
 	//int r6 = co_await silk__spawn( completes_synchronously() );
 
-	silk__coro<int> c2 = silk__spawn( completes_synchronously() );
+	silk::demo_runtime_4_2::task<int> c2 = silk::demo_runtime_4_2::spawn( completes_synchronously() );
 
 	int r6 = co_await c2;
 
-	printf("%d c0() -> %d %d %d %d %d %d %d\n", silk__current_worker_id, r0, r1, r2, r3, r4, r5, r6);
+	printf("%d c0() -> %d %d %d %d %d %d %d\n", silk::current_worker_id, r0, r1, r2, r3, r4, r5, r6);
 
 	count.fetch_add(1, std::memory_order_acquire);
 }
 
 int main() {
-	silk__init_pool(silk__schedule, silk__makecontext);
+	silk::init_pool(silk::demo_runtime_4_2::schedule, silk::makecontext);
 
 	for (int i = 0; i < 1000000; i++) {
-		silk__spawn( c0() );
+		silk::demo_runtime_4_2::spawn( c0() );
 	}
 
-	silk__join_main_thread_2_pool(silk__schedule);
+	silk::join_main_thread_2_pool(silk::demo_runtime_4_2::schedule);
 	
 	sleep(3);
 
